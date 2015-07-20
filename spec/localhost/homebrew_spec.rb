f3be90e1a87d565eca2e73809d3c434fef681faa
Naoto Mawatari <github.mawatari@gmail.com>
2015-07-20 13:34:30 +0900

    ヘルパーおよびテストを作成した

--- spec/localhost/homebrew_spec.rb
+++ spec/localhost/homebrew_spec.rb
@@ -1,2 +1,13 @@
 require 'spec_helper'
 
+homebrew_packages.each do |package|
+  describe package(package) do
+    it { should be_installed }
+  end
+end
+
+homebrew_cask_packages.each do |package|
+  describe file(homebrew_caskroom + '/' + package) do
+    it { should be_directory }
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,26 @@
 require 'serverspec'
+require 'yaml'
 
 set :backend, :exec
 
+def load_configuration (key)
+  configuration = YAML.load_file 'web-development.yml'
+  configuration[0]['vars'][key].map do |package|
+    package.kind_of?(Hash) ? package['name'] : package
+  end
+end
+
+def homebrew_packages
+  load_configuration 'homebrew_packages'
+end
+
+def homebrew_cask_packages
+  load_configuration 'homebrew_cask_packages'
+end
+
+def homebrew_caskroom
+  env = Shellwords.shellsplit(ENV['HOMEBREW_CASK_OPTS'] || '').map do |option|
+    option.split('=')
+  end
+  Hash[*env.flatten]['--caskroom'] || '/opt/homebrew-cask/Caskroom'
+end
